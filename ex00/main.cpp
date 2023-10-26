dc9b9a2ebc219ec496bf203c4d4a573ba1312ebf
Nico Reher <nreher@student.42berlin.de>
2023-10-26 18:14:06 +0200

    improved main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,22 +1,44 @@
 #include "easyfind.hpp"
 #include <iostream>
 #include <vector>
+#include <list>
 
 int main() {
-	std::vector<int> vec;
-	int f;
+	std::cout << "searching in a vector: " << std::endl;
+	{
+		std::vector<int> vec;
+		int f;
 
-	for (int i = 0; i < 12; ++i) {
-		vec.push_back(i);
-	}
-	for (int i = 0; i < 13; ++i) {
-		try {
-			f = easyfind(vec,i);
-			std::cout << f << " found" << std::endl;
+		for (int i = 0; i < 12; ++i) {
+			vec.push_back(i);
 		}
-		catch (notFoundException& e){
-			std::cout << e.what() << std::endl;
+		for (int i = 0; i < 13; ++i) {
+			try {
+				f = easyfind(vec, i);
+				std::cout << f << " found" << std::endl;
+			}
+			catch (notFoundException &e) {
+				std::cout << e.what() << std::endl;
+			}
 		}
 	}
+	//------------------------------------------------------------------//
+	std::cout << "---------------------------------------------------" << std::endl;
+	//------------------------------------------------------------------//
+	std::cout << "searching in a list: " << std::endl;
+	{
+		std::list<int> lst;
 
+		for (int i = 0; i < 12; ++i) {
+			lst.push_back(i);
+		}
+		for (int i = 0; i < 13; ++i) {
+			try {
+				std::cout << easyfind(lst, i) << " found" << std::endl;
+			}
+			catch (notFoundException &e) {
+				std::cout << e.what() << std::endl;
+			}
+		}
+	}
 }
